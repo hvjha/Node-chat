3083c9da0e5dc17858f44e8600c18490cfa7d5b5
hvjha <119492741+hvjha@users.noreply.github.com>
2024-06-29 02:08:59 +0530

    first

--- server/index.js
+++ server/index.js
@@ -6,12 +6,26 @@ const messagesRoute = require("./routes/messagesRoute")
 const socket = require("socket.io")
 const app = express();
 require("dotenv").config();
+const path = require('path')
 
 app.use(cors());
 app.use(express.json());
 app.use("/api/auth",userRoutes);
 app.use("/api/messages",messagesRoute);
+// ============Deployment=============
+const __dirname1 = path.resolve();
+if(process.env.NODE_ENV=="PRODUCTION"){
+    app.use(express.static(path.join(__dirname1,"/client/build")));
 
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname1,"client","index.html"));
+    })
+}else{
+    app.get("/",(req,res)=>{
+        res.send("API is running Successfully")
+    })
+}
+// =================Deployment================
 mongoose.connect(process.env.MONGO_UR||"mongodb+srv://harshvardhanjha35363:12345@cluster0.fqhpsnq.mongodb.net/?retryWrites=true&w=majority").then(()=>{
     console.log("DB is connected Successfully");
 }).catch((err)=>{
